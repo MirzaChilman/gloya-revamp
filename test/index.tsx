c6483a2a31222db6587e6cd18b152131ae730e49
Mirza Chilman <mirzachilman@gmail.com>
2022-09-03 09:51:14 +0700

    init

--- test/index.tsx
+++ test/index.tsx
@@ -1,8 +1,8 @@
 import React, { ReactElement } from "react";
 import {
-    render as baseRender,
-    RenderOptions,
-    RenderResult,
+  render as baseRender,
+  RenderOptions,
+  RenderResult,
 } from "@testing-library/react";
 
 import { ChakraProvider } from "@chakra-ui/react";
@@ -17,22 +17,22 @@ import { QueryClient, QueryClientProvider } from "react-query";
  * please visit https://testing-library.com/docs/react-testing-library/setup
  */
 
-export const AllTheProviders = ({ children }) => {
-    const queryClient = new QueryClient();
+export const AllTheProviders: React.FC = ({ children }) => {
+  const queryClient = new QueryClient();
 
-    return (
-        <>
-            <ChakraProvider theme={theme}>
-                <QueryClientProvider client={queryClient}>
-                    {children}
-                </QueryClientProvider>
-            </ChakraProvider>
-        </>
-    );
+  return (
+    <>
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </ChakraProvider>
+    </>
+  );
 };
 
 const render = (ui: ReactElement, options?: Omit<RenderOptions, "queries">) =>
-    baseRender(ui, { wrapper: AllTheProviders, ...options }) as RenderResult;
+  baseRender(ui, { wrapper: AllTheProviders, ...options }) as RenderResult;
 
 // re-export everything
 export * from "@testing-library/react";
